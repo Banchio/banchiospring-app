f8eba849cbd39eaa17083df081657967794fb5a0
Banchio <simone.banchieri@hotmail.com>
2023-03-03 00:17:46 +0100

    migrated to MSI and added appinsights

--- src/main/java/com/banchio/restapi/RestapiApplication.java
+++ src/main/java/com/banchio/restapi/RestapiApplication.java
@@ -2,11 +2,15 @@ package com.banchio.restapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
+
+
 
 @SpringBootApplication
 public class RestapiApplication {
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(RestapiApplication.class, args);
 	}
 
